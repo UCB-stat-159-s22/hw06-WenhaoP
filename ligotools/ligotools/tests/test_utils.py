9523be1c57e7003b768e7814617cc01ca250e0cd
Wenhao Pan <wenhao1102@berkeley.edu>
2022-04-22 05:51:23 +0000

    Add tests to utils.py“”“”

---
+++ ligotools/ligotools/tests/test_utils.py
@@ -0,0 +1,15 @@
+from ligotools.utils import *
+
+def test_whiten():
+  return
+
+def test_write_wavefile():
+  return
+
+def test_reqshift():
+  return
+  
+def test_plot_changes():
+  return
+
+  
